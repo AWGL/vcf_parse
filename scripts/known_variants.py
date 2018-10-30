cba742531b9d95268b451a6f890f5f3c4b4fc027
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2018-10-30 16:42:38 +0000

    Completed scripts to load and apply known variants.

--- scripts/known_variants.py
+++ scripts/known_variants.py
@@ -13,6 +13,9 @@ Updated:    30 Oct 2018
 """
 
 
+import os
+import csv
+import vcf
 import logging
 
 
@@ -28,17 +31,95 @@ class known_variants:
         self.logger = logging.getLogger('vcf_parse.known')
 
 
-    def load_known_variants(self, in_vcf):
+    def load_known_variants(self, inp):
         """
-        check that there is a Classification column
         Load in vcf and save as list
         """
-        pass
+        # read input vcf with pyvcf package, save as list
+        self.logger.info(
+            'loading known variants from {}'.format(os.path.abspath(inp)))
+
+        with open(inp, 'r') as vcf_input:
+            vcf_reader = vcf.Reader(vcf_input)
+            vcf_list = []
+            vcf_records = []
+            for var in vcf_reader:
+                var_name = '{}:{}{}>{}'.format(
+                    str(var.CHROM), 
+                    str(var.POS), 
+                    str(var.REF), 
+                    str(var.ALT).strip('[]').replace(' ', '')
+                )
+                classification = var.INFO['Classification']
+                vcf_list.append(var_name)
+                vcf_records.append([var_name, classification])
+
+            self.list = vcf_list
+            self.records = vcf_records
+            self.logger.info('loading known variants completed')
 
 
-    def apply_known_variants(self, bedfile, in_vcf, out_folder):
+    def apply_known_variants(self, report):
         """
-        Compares variant id with lost of known variants, annotates if there is a match
+        check that there is a Classification column
+        Compares variant id with list of known variants, annotates if there is a match
         """
-        pass
- 
+        # set report path
+        report_path = report.report_path
+
+        # find classification column in config file
+        if report.annotations:
+            for record in report.annotations:
+                if record[0] == 'Classification':
+                    if record[2] != '':
+                        classification_id = record[2]
+                    else:
+                        classification_id = record[0]
+        else:
+            classification_id = 'Classification'
+
+        if self.list:
+            # open report file and new temp file to save output
+            report_temp = os.path.join(report_path + '.temp')
+            f1 = open(report_path, 'rb')
+            reader = csv.reader(f1, delimiter='\t')
+            f2 = open(report_temp, 'wb')
+            writer = csv.writer(f2, delimiter='\t')
+
+            # load header, 
+            header = next(reader)
+            variant_column = 1
+
+            # find classification column, make one if not present
+            try:
+                classification_column = header.index(classification_id)
+            except:
+                classification_column = len(header)
+                header += ['Classification']
+
+            writer.writerow(header)
+
+            # loop though the report, annotate with classification if there is a match
+            for row in reader:
+                if row[variant_column] in self.list:
+                    i = ''
+                    for r in self.records:
+                        if r[0] == row[variant_column]:
+                            i += '{},'.format(r[1])
+                    writer.writerow(row[0:classification_column] + [i.rstrip(',')] + row[classification_column+1:])
+                else:
+                    writer.writerow(row)
+
+            # tidy up
+            self.logger.info('known variants applied')
+            f1.close()
+            f2.close()
+            os.remove(report_path)
+            os.rename(report_temp, report_path)
+        
+        # if error, skip adding known variants
+        else:
+            self.logger.warn(
+                'could not load known variants file provided, skipping step.'
+            )
+            return
