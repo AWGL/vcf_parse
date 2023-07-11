154b0677137b321e31607b9abba33fd3122c4810
Niamh <niamh.teague@wales.nhs.uk>
2023-07-11 11:59:54 +0100

    for use in SVD

--- vcf_parse.py
+++ vcf_parse.py
@@ -1,313 +1,336 @@
-#!/anaconda3/envs/python2/bin/python
-
 """
-vcf_parse.py
-
-Takes a VCF file and parses the variants to produce a tab delimited 
-variant report.
-
-Usage:  vcf_parse.py [-h] [-v] 
-                     [-O OUTPUT]  
-                     [-t TRANSCRIPTS] [-T TRANSCRIPT_STRICTNESS] 
-                     [-b BED | -B BED_FOLDER] 
-                     [-k KNOWN_VARIANTS]
-                     [-c CONFIG] [-l] 
-                     input
-        vcf_parse.py -h for full description of options.
-
-Author:     Erik Waskiewicz
-Created:    31 Aug 2018
-Version:    0.1.0
-Updated:    31 Oct 2018
+Convert annotated vcf from 1_SomaticAmplicon.sh into readable variant report file
 """
-__version__ = '0.1.0'
-__updated__ = '31 Oct 2018'
-
-
-import argparse
+import re
+import os
+import csv
 import logging
+import argparse
 import textwrap
+from pysam import VariantFile
+from pyvariantfilter.utils import parse_csq_field
 
-from scripts.vcf_report import vcf_report
-from scripts.preferred_transcripts import preferred_transcripts
-from scripts.bed_object import bed_object
-from scripts.known_variants import known_variants
+# Main functions to create variant report file
+#	"""
+#	Logger deals with all status messages
+#	"""
+
+logger = logging.getLogger('vcf_parse.vcf')
+
+def parse_transcripts(pref_trans):
+	"""
+	Get list of preferred transcripts
+	"""
+
+	#Empty variable
+	pref_transcript = []
+
+	#Make list of transcripts
+	trans = open(pref_trans, 'r')
+
+	for line in trans:
+
+		#Skip header
+		if line.startswith('Transcript'):
+			next
+		else:
+			transcript = line.strip().split('\t')[0]
+			pref_transcript.append(transcript)
+	trans.close()
+
+	return(pref_transcript)
+
+	logger.info('Loaded preferred transcripts')
+
+def parse_ntc(ntc_file):
+	"""
+	Create dictionary of all NTC variants
+	"""
+
+	# Empty ntc list
+	ntc_list = []
+
+	# Loop over the ntc file
+	ntc = VariantFile(ntc_file)
+
+	for line in ntc.fetch():
+
+		chrom = line.chrom
+		pos = line.pos
+		ref = line.ref
+		alt = line.alts[0]
+		var = chrom+":"+str(pos)+ref+">"+alt
+
+		vaf = line.samples[0]['AF'][0]
+		dp = line.samples[0]['DP']
+
+		#For alt read count, get last value from AD
+		alt = line.samples[0]['AD'][1]
+
+		ntc_var = [var, vaf, dp, alt]
+		ntc_list.append(ntc_var)
+
+	return(ntc_list)
+
+	logger.info('created list of NTC variants')
+
+def parse_sample(inp, outfile, ntc_list, pref_transcript):
+	"""
+	Loads in data from vcf
+	Returns list of variants in correct format
+	Only gives ntc_vaf and ntc_depth if in_ntc = True
+	"""
+
+	# Read filtered meta annotated vcf produced in 1_SomaticAmplicon.sh
+	logger.info(
+		'loading VCF file from {}'.format(os.path.abspath(inp)))
+
+	# Empty list to hold variants
+	variant_list = []
+
+	# Read vcf file
+	inp = VariantFile(inp)
+
+	# CSQ fields from header to be parsed with pyvariantfilter
+	csq_fields = str(inp.header.info['CSQ'].record)
+	csq_fields = csq_fields.strip()
+	index = csq_fields.index('Format') + 8
+	csq_fields = csq_fields[index:len(csq_fields)-2].split('|')
+
+	for record in inp.fetch():
+
+		if 'CSQ' in record.info:
+
+			# Load chrom, pos, ref & alt fields
+			chrom = record.chrom
+			pos = record.pos
+			ref = record.ref
+			alt = record.alts[0]
+
+			# Load vaf and depth
+			vaf = record.info['AF']
+			#vaf = record.samples[0]['AF'][0]
+			#depth = record.samples[0]['DP'][0]
+			depth = record.info['DP']
+			# Alt-reads - take second value from AD
+			alt_reads = record.samples[0]['AD'][1]
+
+			# Sample for output filepath
+
+			# Get annotations and go through these
+			csq = record.info['CSQ']
+			annotations = parse_csq_field(csq, csq_fields)
+
+			# Empty list of annotations
+			transcripts = []
+
+			# If any of the 'Feature' values match the preferred transcript
+			for entry in annotations:
+
+				# Loop over annotations and if preferred transcript add to transcript list
+				if entry['Feature'] in pref_transcript:
+					gene = entry['SYMBOL']
+
+			# If transcripts for variant still empty, add MANE select
+			if len(transcripts) == 0:
+
+				for entry in annotations:
+
+					if entry['MANE']:
+
+						gene = entry['SYMBOL']
+						hgvs_p = entry['HGVSp']
+						hgvs_c = entry['HGVSc']
+						consequence = entry['Consequence']
+						exon = entry['EXON']
+
+						transcript_info = (gene
+								, hgvs_p
+								, hgvs_c
+								, consequence
+								, exon)
+
+						transcripts.append(transcript.info)
+
+			# If transcripts still empty, use PICK annotation
+			if len(transcripts) == 0:
+
+				for entry in annotations:
+
+					if 'PICK':
+
+						gene = entry['SYMBOL']
+						hgvs_p = entry['HGVSp']
+						hgvs_c = entry['HGVSc']
+						consequence = entry['Consequence']
+						exon = entry['EXON']
+
+						transcript_info = (gene
+								, hgvs_p
+								, hgvs_c
+								, consequence
+								, exon)
+
+						transcripts.append(transcript_info)
+
+			# If still empty leave empty
+			if len(transcripts) == 0:
+
+				gene = ""
+				hgvs_p = ""
+				hgvs_c = ""
+				consequence = ""
+				exon = ""
+
+				transcript_info = (gene
+						, hgvs_p
+						, hgvs_c
+						, consequence
+						, exon)
+
+				transcripts.append(transcript.info)
+
+			#Check if NTC variant list empty
+			if len(ntc_list) == 0:
+				in_ntc = ""
+				ntc_vaf = ""
+				ntc_depth = ""
+				ntc_alt_reads = ""
+			else:
+				#Check if variant in NTC
+				for entry in ntc_list:
+					if variant_full == entry[0]:
+						in_ntc = "True"
+						ntc_vaf = entry[1]
+						ntc_depth = entry[2]
+						ntc_alt_reads = entry[3]
+					else:
+						in_ntc = "False"
+						ntc_vaf = ""
+						ntc_depth = ""
+						ntc_alt_reads = ""
+
+			#Go through all annotations we're keeping for this variant and add them
+			for keep in transcripts:
+				var = f'{keep[0]}\t{chrom}\t{pos}\t{ref}\t{alt}\t{vaf}\t{depth}\t{keep[1]}\t{keep[2]}\t{keep[3]}\t{keep[4]}\t{alt_reads}\t{in_ntc}\t{ntc_vaf}\t{ntc_depth}\t{ntc_alt_reads}'
+				variant_list.append(var)
 
 
-## -- PARSE INPUT ARGUMENTS -------------------------------------------
+	return(variant_list)
 
+		# load output filepath
+#		if out is not None:
+#			output_dir = os.path.abspath(out)
+#		else:
+#			output_dir = os.path.abspath('.')
+#
+#		report_path = os.path.join(
+#			output_dir, sample + '_VariantReport.txt')
+#
+#		logger.info('loading VCF complete')
+
+## Input arguments
 def get_args():
-    """
-    Use argparse package to take arguments from the command line. 
-    See descriptions for full detail of each argument.
-    """
+	"""
+	Uses argparse to take arguments from command line
+	"""
 
-    # Make argparse object, add description
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter,
-        description=textwrap.dedent(
-        '''
-        summary:
-        Takes a VCF file and parses the variants to produce a tab delimited 
-        variant report.
-        '''
-    ))
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawTextHelpFormatter,
+		description=textwrap.dedent(
+		'''
+		Summary:
+		Takes VCF file created by 1_SomaticAmplicon.sh and parses
+		the variants to produce a tab delimited variant report
+                '''
+	))
 
+	# Required: VCF input file
+	parser.add_argument('-i', '--inp', action='store',
+		help ='Filepath to input VCF file. REQUIRED.'
+	)
 
-    # Version info
-    parser.add_argument(
-        '-v', '--version', action='version', 
-        version=
-            '%(prog)s\nversion:\t{}\nlast updated:\t{}'.format(
-                __version__, __updated__
-    ))
+	# OPTIONAL: Output folder, defaults to current directory if empty
+	parser.add_argument(
+		'-o', '--outfile', action='store', 
+		help=textwrap.dedent(
+		'''
+		Filepath to folder where output reports will be saved. 
+		If missing, defaults to current directory.
+		\n'''
+	))
 
+	# Required: Preferred transcript file
+	parser.add_argument(
+		'-t', '--pref_trans', action='store',
+		help='Filepath to preferred transcrips'
+	)
 
-    # Arguments (see help string for full descriptions):
-    # REQUIRED: VCF file input
-    parser.add_argument(
-        'input', action='store', 
-        help='Filepath to input VCF file. REQUIRED.'
-    )
+	# Required: Psth to ntc vcf file
+	parser.add_argument(
+		'-n', '--ntc_vcf', action='store',
+		help='Filepath to NTC VCF'
+	)
 
+	return parser.parse_args()
 
-    # OPTIONAL: Output folder, defaults to current directory if empty
-    parser.add_argument(
-        '-O', '--output', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to folder where output reports will be saved. 
-        If missing, defaults to current directory.
-        \n'''
-    ))
-
-
-    # OPTIONAL: List of preferred transcripts
-    parser.add_argument(
-        '-t', '--transcripts', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to preferred transcripts file. 
-
-        Must be a tab seperated file with preferred transcripts in the second 
-        column. If missing, all entries in the preferred transcript column 
-        will be labelled as 'Unknown'.
-        \n'''
-    ))
-
-
-    # OPTIONAL: Preferred transcripts strictness
-    parser.add_argument(
-        '-T', '--transcript_strictness', action='store', default='low', 
-        help=textwrap.dedent(
-        '''
-        Strictness of matching while annotating preferred transcripts.
-        Default setting is low.
-
-        Options: 
-
-        high - Transcripts must be an exact match. 
-               e.g. NM_001007553.2 and NM_001007553.1 won't match,
-                    NM_001007553.1 and NM_001007553.1 will.
-
-        low  - Transcripts will match regardless of the version number. The 
-               version number is after the . at the end of a transcript 
-               e.g. NM_001007553.2 and NM_001007553.1 will match.
-        \n'''
-    ))
-
-
-    # OPTIONAL: either a single BED file or a folder containing BED 
-    # files, only one of these can be used
-    bed_files = parser.add_mutually_exclusive_group()
-
-    # Single BED file
-    bed_files.add_argument(
-        '-b', '--bed', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to a single BED file. 
-
-        The BED file will be applied to the variant report and a seperate
-        report saved with the BED file applied. This report will be saved in 
-        the same output folder as the original variant report, with the BED 
-        file name added to it.
-        Cannot be used together with -B flag.
-        \n'''
-            ))
-
-    # Multiple BED files
-    bed_files.add_argument(
-        '-B', '--bed_folder', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to folder containing BED files. 
-
-        Each BED file will be applied to the variant report and a seperate
-        report saved with the BED file applied. These reports will be saved in
-        a new folder within the output folder, named the same as the input BED
-        folder. 
-        The file names will be the same as the original variant report, with 
-        the BED file name added to them.
-        Cannot be used together with -b flag.
-        \n'''
-    ))
-
-
-    # OPTIONAL: File containing known variants
-    parser.add_argument(
-        '-k', '--known_variants', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to known variants file. 
-
-        This is a VCF file containing any known variants and an associated 
-        classification. The classification will be added to the variant 
-        report. The VCF must have an annotation named 'Classification' within 
-        the INFO field for each variant.
-
-        Key:
-        0 - Artifact
-        1 - Benign
-        2 - Likely benign
-        3 - VUS
-        4 - Likely pathogenic
-        5 - Pathogenic
-        \n'''
-    ))
-
-
-    # OPTIONAL: File containing the headers for the report
-    parser.add_argument(
-        '-c', '--config', action='store', 
-        help=textwrap.dedent(
-        '''
-        Filepath to config file. 
-
-        This is a tab seperated text file containing a number of rows, where 
-        each row specifies an annotation to be included in the variant report.
-        Only annotations included in the config file will be included in the
-        variant report.
-        The columns in the variant report will be in the same order as the 
-        order in which the annotations appear in the config file.
-
-        Each row contains:
-
-        Column 1 - Required. Annotation headers, these must match up with how
-                   they appear in the VCF (case sensitive).
-
-        Column 2 - Required. Location where to find the data within the VCF, 
-                   used to select the correct parsing function.
-                   options: info, format, vep, filter or pref.
-
-        Column 3 - Optional. Alternative name for column header.
-
-        To make a config file with all available options from a VCF, run:
-            vcf_parse -l path_to_input_vcf > config.txt
-        \n'''
-    ))
-
-
-    # OPTIONAL: Lists all headers in a vcf then exits
-    parser.add_argument(
-        '-l', '--config_list', action='store_true', 
-        help=textwrap.dedent(
-        '''
-        Return a list of all availabile config to the screen, then exit.
-        See CONFIG section for usage.
-        \n'''
-    ))
-
-
-    # OPTIONAL: Filter out any variants where FILTER column is not PASS
-    parser.add_argument(
-        '-F', '--filter_non_pass', action='store_true', 
-        help=textwrap.dedent(
-        '''
-        Filters out any variants where the FILTER annotation is not 
-        PASS. If missing then there will be no fitering based on the
-        FILTER annotation.
-        \n'''
-    ))
-
-    return parser.parse_args()
-
-
-# -- MAIN FUNCTION ----------------------------------------------------
-
+# Function to run main functions
 def main(args):
-    # setup logger
-    logger = logging.getLogger('vcf_parse')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        '%(levelname)s\t%(asctime)s\t%(name)s\t%(message)s'
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.info('running vcf_parse.py...')
 
-    # Load arguments, make vcf report object and load data
-    report = vcf_report()
-    report.load_data(args.input, args.output)
+	# Setup logger
+	logger = logging.getLogger('vcf_parse')
+	logger.setLevel(logging.DEBUG)
 
-    # If -l flag called, print headers and exit
-    if args.config_list:
-        report.list_config()
-        exit()
+	handler = logging.StreamHandler()
+	handler.setLevel(logging.DEBUG)
 
-    # If config file provided, load config
-    if args.config:
-        report.load_config(args.config)
-    else:
-        logger.info('no config file found -- outputting all data from VCF.')
+	formatter = logging.Formatter(
+		'%(levelname)s\t%(asctime)s\t%(name)s\t%(message)s'
+	)
 
-    # Make variant report of whole VCF
-    report.make_report(args.filter_non_pass)
+	handler.setFormatter(formatter)
 
-    # If preferred transcripts provided, apply to variant report
-    if args.transcripts:
-        pt = preferred_transcripts()
-        pt.load(args.transcripts)
-        pt.apply(report, args.transcript_strictness)
-    else:
-        logger.info('no preferred transcripts file provided -- preferred ' +
-        'transcripts column will all be labelled as "Unknown"')
+	logger.addHandler(handler)
+	logger.info('running vcf_parse2.py...')
 
-    # If known variants provided, apply to variant report
-    if args.known_variants:
-        known = known_variants()
-        known.load_known_variants(args.known_variants)
-        known.apply_known_variants(report)
-    
-    else:
-        logger.info('no known variants file provided -- Classification ' +
-        'column will be empty')
+	#Load arguments, make vcf report object and load data
 
-    # If single BED file provided, make variant report with BED file 
-    # applied
-    if args.bed:
-        bed = bed_object()
-        bed.apply_single(args.bed, report)
+	# Parse preferred transcripts
+	if args.pref_trans:
+		pref_transc = parse_transcripts(args.pref_trans)
+	else:
+		logger.info('no preffered transcript file provided - please provide')
 
-    # If folder of BED file provided, make a seperate variant report 
-    # for each BED file. Output will be saved in a folder named the 
-    # same as the BED file folder, within the output directory.
-    elif args.bed_folder:
-        bed = bed_object()
-        bed.apply_multiple(args.bed_folder, report)
+	# Parse NTC
+	if args.ntc_vcf:
+		ntc_list = parse_ntc(args.ntc_vcf)
+	else:
+		logger.info('no ntc vcf provided - please provide')
 
-    # If no BED files provided, pass
-    else:
-        logger.info('no BED files provided')
+	# Parse sample
+	variant_list = parse_sample(args.inp, args.outfile, ntc_list, pref_transc)
 
-    # Finish
-    logger.info('vcf_parse.py completed\n{}'.format('---'*30))
+	# Finish
+	logger.info('steps completed\n{}'.format('---'*30))
 
+	# Export data
+	# Header lines
+	logger.info('creating output file')
 
-# -- CALL FUNCTIONS ---------------------------------------------------
+	outfile_headers = ['gene', 'chr', 'pos', 'ref', 'alt', 'vaf', 'depth', 'hgvs_p', 'hgvs_c', 'consequence', 'exon', 'alt_reads', 'in_ntc', 'ntc_vaf', 'ntc_depth', 'ntc_alt_reads']
+	outfile_headers_string = '\t'.join(outfile_headers)
 
+	#Info
+	with open(args.outfile,'w',newline='') as file:
+		file.write(outfile_headers_string + '\n')
+
+		for line in variant_list:
+			file.write(str(line) + '\n')
+
+	# Final file
+	logger.info('vcf_parse2.py completed\n{}'.format('---'*30))
+
+# Call functions
 if __name__ == '__main__':
-    args = get_args()
-    main(args)
+	args = get_args()
+	main(args)
